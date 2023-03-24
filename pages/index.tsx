acff04a9de210f1758a11d72869527ac9e73d74d
Hayden Thai <thaihayden@gmail.com>
2023-03-23 22:41:49 -0700

    basic chat app uses NextJS and OpenAI ChatGPT

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import { Layout, Text, Page } from '@vercel/examples-ui'
+import { Chat } from '../components/Chat'
+
+function Home() {
+  return (
+    <Page className="flex flex-col gap-12">
+      <section className="flex flex-col gap-6">
+        <Text variant="h1">CougarGPT</Text>
+        <Text className="text-zinc-600">
+          A simple chat app made by Hayden Thai.
+        </Text>
+      </section>
+
+      <section className="flex flex-col gap-3">
+        <Text variant="h2">CougarGPT:</Text>
+        <div className="lg:w-2/3">
+          <Chat />
+        </div>
+      </section>
+    </Page>
+  )
+}
+
+Home.Layout = Layout
+
+export default Home
